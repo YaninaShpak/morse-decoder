d778f6f415f4e36b5e74ae6e2900590029c18d8c
Yanina <95853977+YaninaShpak@users.noreply.github.com>
2022-12-30 16:52:05 +0300

    write a function, that decodes Morse code

--- src/index.js
+++ src/index.js
@@ -38,7 +38,32 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let arr = [];
+
+  for (let i = 0; i < expr.length; i += 10) {
+    arr.push(expr.slice(i, i + 10));
+  }
+
+  let newArr = arr.map((el) => {
+    let str = '';
+    for (let i = 0; i < el.length; i += 2) {
+      if (el[i] === '1' && el[i + 1] === '0') {
+        str = str + '.'
+      }
+
+      if (el[i] === '1' && el[i + 1] === '1') {
+        str = str + '-'
+      }
+    }
+
+    return str;
+  });
+
+  let decArr = newArr.map((el) => {
+    return el === '' ? ' ' : MORSE_TABLE[el];
+  });
+
+  return decArr.join('');
 }
 
 module.exports = {
